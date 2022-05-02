5b13bf2cc5e14d2401bed37d4e75b89fe0041a0f
Chris Johns <chrisjohns.me@gmail.com>
2022-05-02 17:44:06 -0600

    Account stats - Added average RP stat on dashboard

--- projects/allfather-app/src/app/modules/main/pages/dashboard/components/account-stats-display.component.ts
+++ projects/allfather-app/src/app/modules/main/pages/dashboard/components/account-stats-display.component.ts
@@ -1,8 +1,14 @@
 import { PlayerAccountStats } from "@allfather-app/app/common/player-account-stats";
+import { MatchDataStore } from "@allfather-app/app/modules/core/local-database/match-data-store";
+import { MatchService } from "@allfather-app/app/modules/core/match/match.service";
 import { MyPlayerAccountStatsService } from "@allfather-app/app/modules/core/player-account-stats/my-player-account-stats.service";
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from "@angular/core";
+import { isEmpty, mathAverageRate } from "common/utilities";
+import { isDate } from "date-fns";
 import { Subject } from "rxjs";
-import { takeUntil } from "rxjs/operators";
+import { switchMap, takeUntil } from "rxjs/operators";
+
+const AVG_RP_MATCH_LIMIT = 10;
 
 @Component({
     selector: "app-account-stats-display",
@@ -11,15 +17,30 @@ import { takeUntil } from "rxjs/operators";
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AccountStatsDisplayComponent implements OnInit, OnDestroy {
+    /** Per ranked game */
+    public averageRPRate?: number;
     public Infinity = Infinity;
     public myAccountStats?: PlayerAccountStats;
 
     private destroy$ = new Subject<void>();
 
-    constructor(private readonly cdr: ChangeDetectorRef, private readonly myPlayerAccountStats: MyPlayerAccountStatsService) {}
+    constructor(
+        private readonly cdr: ChangeDetectorRef,
+        private readonly match: MatchService,
+        private readonly myPlayerAccountStats: MyPlayerAccountStatsService
+    ) {}
 
     public ngOnInit(): void {
         this.setupStatsEventListener();
+        this.setupLiveMatchListeners();
+
+        this.match
+            .getAllMatchData$()
+            .pipe(takeUntil(this.destroy$))
+            .subscribe((matchList) => {
+                this.averageRPRate = this.calcAverageRPRate(matchList);
+                this.refreshUI();
+            });
     }
 
     public ngOnDestroy(): void {
@@ -29,13 +50,41 @@ export class AccountStatsDisplayComponent implements OnInit, OnDestroy {
 
     /** Strips out spaces, special characters, lower-cases input */
     public cleanCSSClassName(input?: string): string {
-        return `${input?.toLowerCase().replace(/[_\W]+/, "") ?? ""}`;
+        return input?.toLowerCase().replace(/[_\W]+/, "") ?? "";
+    }
+
+    /** Calculates the Average Rank Score gained/lost per hour. */
+    private calcAverageRPRate(matchList: MatchDataStore[]): Optional<number> {
+        const rankedMatches = matchList
+            .filter((match) => !isEmpty(match.rankScore) && !!match.endDate && isDate(match.endDate))
+            .slice(0, AVG_RP_MATCH_LIMIT);
+        const rankedScores: number[] = rankedMatches.map((match) => match.rankScore!);
+        if (rankedScores.length < 2) return;
+
+        const avgRPRate = mathAverageRate(rankedScores);
+        return avgRPRate;
     }
 
     private setupStatsEventListener(): void {
         this.myPlayerAccountStats.myAccountStats$.pipe(takeUntil(this.destroy$)).subscribe((myAccountStats) => {
             this.myAccountStats = myAccountStats;
-            this.cdr.detectChanges();
+            this.refreshUI();
         });
+    }
+
+    private setupLiveMatchListeners(): void {
+        this.match.onMatchDataStoreChanged$ // Match was updated or added to local database
+            .pipe(
+                takeUntil(this.destroy$),
+                switchMap(() => this.match.getAllMatchData$())
+            )
+            .subscribe((matchList) => {
+                this.averageRPRate = this.calcAverageRPRate(matchList);
+                this.refreshUI();
+            });
+    }
+
+    private refreshUI(): void {
+        this.cdr.detectChanges();
     }
 }
